f2fff89a9aa80fee50590dbe5dfd0201eff0afc0
James W <nc@m3s.uk>
2023-10-13 04:23:54 +0100

    refactored var names

--- MountBuddy.lua
+++ MountBuddy.lua
@@ -1,9 +1,9 @@
 local MountBuddy = {
   ADDON_NAME = "MountBuddy",
   SAVED_VAR_NAME = "MountBuddySavedVariables",
-  DEFAULT = { TRAINING_ORDER = { "speed", "stamina", "carrying" } },
-  VARIABLE_VERSION = 1,
-  DB = {},
+  SAVED_VAR_DEFAULTS = { TRAINING_ORDER = { "speed", "stamina", "carrying" } },
+  SAVED_VARIABLES = {},
+  SAVED_VAR_VERSION = 1,
   HELP_TEXT = [[
 
     /mountbuddy - Prints this help information.
@@ -34,21 +34,21 @@ end
 --set which mount training to do
 function MountBuddy:setTrainingOrder(input)
   if not self.TRAINING_LOOKUP[input] then
-    local training_options = "Training Order options are:\n %s"
+    local trainOptions = "Training Order options are:\n %s"
     local trainTypes = ""
     for key, order in pairs(self.TRAINING_LOOKUP) do
       trainTypes = trainTypes .. key .. ". " .. table.concat(order, ',') .. "\n"
     end
-    d(string.format(training_options, trainTypes))
+    d(string.format(trainOptions, trainTypes))
     return
   end
-  self.DB.TRAINING_ORDER = self.TRAINING_LOOKUP[input]
+  self.SAVED_VARIABLES.TRAINING_ORDER = self.TRAINING_LOOKUP[input]
   MountBuddy:printTrainingOrder()
 end
 
 --print the current mount training
 function MountBuddy:printTrainingOrder()
-  local strOrder = table.concat(self.DB.TRAINING_ORDER, ',')
+  local strOrder = table.concat(self.SAVED_VARIABLES.TRAINING_ORDER, ',')
   d(string.format("Training Order set to: %s", strOrder))
 end
 
@@ -63,7 +63,7 @@ function MountBuddy:skipChat(_, optionCount)
 end
 
 function MountBuddy:trainMount()
-  for _, trainType in ipairs(self.DB.TRAINING_ORDER) do
+  for _, trainType in ipairs(self.SAVED_VARIABLES.TRAINING_ORDER) do
     local control = GetControl(self.BUTTON_NAMES[trainType])
     ZO_Stable_TrainButtonClicked(control)
   end
@@ -75,7 +75,7 @@ function MountBuddy:onAddonLoad(event, addonName)
   --exit if not the correct addon
   if addonName ~= self.ADDON_NAME then return end
   d(string.format("%s loaded.", self.ADDON_NAME))
-  self.DB = ZO_SavedVars:NewCharacterIdSettings(self.SAVED_VAR_NAME, self.VARIABLE_VERSION, nil, self.DEFAULT)
+  self.SAVED_VARIABLES = ZO_SavedVars:NewCharacterIdSettings(self.SAVED_VAR_NAME, self.SAVED_VAR_VERSION, nil, self.SAVED_VAR_DEFAULTS)
   EVENT_MANAGER:UnregisterForEvent(self.ADDON_NAME, EVENT_ADD_ON_LOADED)
 end
 
